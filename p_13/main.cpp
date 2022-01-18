1b6f2b046bf4fad494aa43b47ad544e0b379002a
irinel132 <irinel132@gmail.com>
2022-01-18 01:47:24 +0000

    Fixed P 13

--- p_13/main.cpp
+++ p_13/main.cpp
@@ -3,36 +3,32 @@
 
 using namespace std;
 
-int n = 100,
-	a[100][50],
-	x[100];
+int x[100];
 
-void print_arr() {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < 50; j++) {
-			printf("%d", a[i][j]);
-		}
-		printf("\n");
-	}
-}
 
 int main() {
 	ifstream in("num.txt");
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < 50; j++) {
+	for (int i = 0; i < 100; i++) {
+		for (int j = 49; j >= 0; j--) {
 			char c;
 			in >> c;
-			a[i][j] = c - 48;
-		}
-	}
-	
-	for (int j = 0; j < 50; j++) {
-		for (int i = 0; i < 100; i++) {
-			x[j] += a[i][j] ;
+			x[j] += (c - 48);
+
 		}
 	}
 
-	print_arr();
+	for (int i = 0; i < 99; i++)	 {
+		x[i+1] += x[i] / 10;
+		x[i] = x[i] % 10;
+
+	}
+
+	int i = 100;
+	while (!x[--i]);
+	for (int j = 0; j < 10; j++) {
+		printf("%d", x[i - j]);
+	}
+	printf("\n");
 
 	return 0;
 }
